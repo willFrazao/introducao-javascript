6896cbaecd3cf2431489ec072df4d8fc3602ab04
william <william.frazao@original.io>
2019-12-07 09:46:47 -0300

    calculo de variaveis

---
+++ js/principal.js
@@ -0,0 +1,32 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso ínvalido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
